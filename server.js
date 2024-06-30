eaf9c3b733608d8c6569e170dc8dac09819644a2
Prashanth G <prashanthg300@gmail.com>
2024-06-30 21:33:02 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,26 +1,29 @@
-const express = require("express")
-const cors = require('cors')
+const express = require("express");
+const cors = require('cors');
 require('dotenv').config();
-const { connectTOMongoDB } = require("./connect")
-const app = express()
+const { connectTOMongoDB } = require("./connect");
+const app = express();
 const bodyParser = require('body-parser');
-const mongoURI = process.env.MONGO_URI
 
-const PORT = 5000
-const apiRoutes = require('./routes/review')
-app.use(express.json())
+const mongoURI = process.env.MONGO_URI;
+const PORT = process.env.PORT || 5000;
+const apiRoutes = require('./routes/review');
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ origin: "https://moengage-brewery-r-kamalnath.onrender.com" }))
-connectTOMongoDB(mongoURI)
+app.use(cors({ origin: "https://moengage-brewery-r-kamalnath.onrender.com" }));
 
-app.get("/",(req,res)=>{
-    res.send("All is well!")
-} );
-app.use("/api",apiRoutes);
+// Connect to MongoDB
+connectTOMongoDB(mongoURI);
 
+// Test Route
+app.get("/", (req, res) => {
+    res.send("All is well!");
+});
 
-
+// API Routes
+app.use("/api", apiRoutes);
 
 app.listen(PORT, () => {
-    console.log("Server is running at", PORT);
+    console.log(`Server is running at port ${PORT}`);
 });
